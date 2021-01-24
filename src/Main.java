bb6afd5c7564feeda61cdbe0e3d539f6475bf85b
Vladimir Shcherban <vladimir.shcherban@yandex.ru>
2021-01-24 13:21:33 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        Cat bigCat = new Cat(5500, "Murka", 2, "Black");
-        Cat smallCat = new Cat(1000, "Kuzya", 1, "White");
+        Cat bigCat = new Cat(5500, "Murka_Second commit", 2, "Black");
+        Cat smallCat = new Cat(1000, "Kuzya_Second commit", 1, "White");
         bigCat.weight += 100;
         smallCat.weight += 1000;
         System.out.println("Вес большой кошки = " + bigCat.weight);
